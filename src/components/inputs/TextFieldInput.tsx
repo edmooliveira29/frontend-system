2f347c8da3ba9a20c38e42f57526302220d4bfac
edmooliveira29 <edmooliveira29@gmail.com>
2023-04-26 08:03:47 -0300

    fix: create responsibility for input

--- src/components/inputs/TextFieldInput.tsx
+++ src/components/inputs/TextFieldInput.tsx
@@ -4,7 +4,7 @@ import TextField from '@mui/material/TextField';
 export function TextFieldInput(props: {label: string; typeInput: string}): React.ReactElement {
 	return (
 		<TextField
-			sx={{'& > :not(style)': {m: 1, width: '23em', borderRadius: '5px'}}}
+			sx={{'& > :not(style)': {m: 1, width: '100%', borderRadius: '5px', minWidth: 'clamp(0px, ($targetWidth - 100%) * 999, 100%)'}}}
 			id={`text-field-${props.label}`}
 			type={props.typeInput}
 			label={props.label}

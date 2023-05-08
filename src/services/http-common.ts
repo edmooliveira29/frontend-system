1a39591bcb81ab24e12881e5fd30428b9c0c8597
edmooliveira29 <edmooliveira29@gmail.com>
2023-05-08 20:59:22 -0300

    feat: disable cert ssl

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -1,6 +1,13 @@
 import axios from 'axios';
+import https from 'https';
+const agent = new https.Agent({
+	rejectUnauthorized: false,
+	requestCert: false });
 
 export default axios.create({
 	baseURL: 'https://34.139.16.117:5000/v1/api',
+	responseType: 'json',
+    withCredentials: true,
+    httpsAgent: agent
 });
 

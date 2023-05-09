6eb69de87458ea505452629c4c06b3031db9aa16
edmooliveira29 <edmooliveira29@gmail.com>
2023-05-08 21:45:06 -0300

    feat: disabled connection ssl

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -1,13 +1,10 @@
 import axios from 'axios';
-import https from 'https';
-const agent = new https.Agent({
-	rejectUnauthorized: false,
-	requestCert: false });
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 export default axios.create({
 	baseURL: 'https://34.139.16.117:5000/v1/api',
 	responseType: 'json',
     withCredentials: true,
-    httpsAgent: agent
 });
 

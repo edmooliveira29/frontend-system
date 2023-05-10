9047b189702ad27362f3af2ffe0067f16588b896
edmooliveira29 <edmooliveira29@gmail.com>
2023-05-10 09:56:18 -0300

    fix: add .env with baseUrl

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -1,10 +1,11 @@
-import axios from 'axios';
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+import axios from 'axios'
+const environment = process.env.NODE_ENV
+const baseURLDevelopment = process.env.BASE_URL_DEVELOPMENT
+const baseURLProduction = process.env.BASE_URL_PRODUCTION
 
 export default axios.create({
-	baseURL: 'https://api.edmopuc.online/v1/api',
+	baseURL: environment === 'production' ? baseURLProduction : baseURLDevelopment,
 	responseType: 'json',
-    withCredentials: true,
+	withCredentials: true,
 });
 

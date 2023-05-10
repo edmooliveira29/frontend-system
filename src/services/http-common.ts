77504515b9f4f84774cf0671fc181e74e0de743c
edmooliveira29 <edmooliveira29@gmail.com>
2023-05-10 10:21:48 -0300

    fix: verify erro with enviroment

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -2,7 +2,9 @@ import axios from 'axios'
 const environment = process.env.NODE_ENV
 const baseURLDevelopment = process.env.BASE_URL_DEVELOPMENT
 const baseURLProduction = process.env.BASE_URL_PRODUCTION
-
+console.log(environment)
+console.log(baseURLDevelopment)
+console.log(baseURLProduction)
 export default axios.create({
 	baseURL: environment === 'production' ? baseURLProduction : baseURLDevelopment,
 	responseType: 'json',

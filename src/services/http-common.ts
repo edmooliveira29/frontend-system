a59ea6300a8d8be00a60c090f9656e58d9001f4b
edmooliveira29 <edmooliveira29@gmail.com>
2023-05-10 10:38:36 -0300

    fix: remove console.log

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -3,9 +3,7 @@ import axios from 'axios'
 const environment = process.env.NODE_ENV
 const baseURLDevelopment = process.env.REACT_APP_BASE_URL_DEVELOPMENT
 const baseURLProduction = process.env.REACT_APP_BASE_URL_PRODUCTION
-console.log(environment)
-console.log(baseURLDevelopment)
-console.log(baseURLProduction)
+
 export default axios.create({
 	baseURL: environment === 'production' ? baseURLProduction : baseURLDevelopment,
 	responseType: 'json',

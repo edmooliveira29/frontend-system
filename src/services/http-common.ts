d3493144a0beee1bac0488479ac7df9c2e7b508d
edmooliveira29 <edmooliveira29@gmail.com>
2023-05-09 21:26:35 -0300

    fix: fix baseurl to api

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 export default axios.create({
-	baseURL: 'https://34.139.16.117:5000/v1/api',
+	baseURL: 'https://api.edmopuc.online/v1/api',
 	responseType: 'json',
     withCredentials: true,
 });

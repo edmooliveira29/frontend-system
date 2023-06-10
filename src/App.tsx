62e8b484082719d9495f1028057d64be8a439058
edmooliveira29 <edmooliveira29@gmail.com>
2023-06-10 18:52:05 -0300

    fix: put githubactions with netclify

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { router } from './router/router'
 
-function App() {
-	return router
-}
+function App() { return router }
 
 export default App

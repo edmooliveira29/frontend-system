5d08591f80825028425911ceb6ce88b1dcd20584
edmooliveira29 <edmooliveira29@gmail.com>
2023-08-16 14:03:33 -0300

    feat: setup cypress to running tests e2e

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "cypress"
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000',
+    supportFile:'tests/e2e/cypress/support/e2e.ts',
+    specPattern:'tests/e2e/cypress/e2e/**.cy.ts'
+  },
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    }
+  },
+})
---
+++ tests/e2e/cypress/e2e/edit-my-account.cy.ts
@@ -0,0 +1,16 @@
+describe('Edit my account', (): void => {
+  it('enter website', () => {
+    cy.viewport(1366, 768)
+    cy.visit('http://localhost:3000/')
+    cy.get('.swal2-confirm').click()
+    cy.get('#link-login').click()
+    cy.get('#button-login').click({ force: true })
+    cy.get('#img-user').click()
+    cy.get('#my-account-dropdown').click()
+
+    //expect(true).to.equal(true)
+  })
+  it('enter my account', () => {
+  })
+
+})

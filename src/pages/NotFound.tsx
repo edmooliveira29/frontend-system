273974c5d7b032de4a9e93b0517361c2c3eff715
edmooliveira29 <edmooliveira29@gmail.com>
2023-08-16 16:53:34 -0300

    fix: update route 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,24 +1,22 @@
 import React from 'react'
-import { ComponentButtonCommon, Footer, NavBar } from '../components'
+import { ComponentButtonCommon } from '../components'
 import { Link } from 'react-router-dom'
 
 export const NotFound = () => (
   <>
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <NavBar />
-      <div className="d-flex align-items-center justify-content-center vh-100">
+    <div style={{ display: 'flex', flexDirection: 'column'}}>
+      <div className="d-flex align-items-center justify-content-center p-5">
         <div className="text-center">
           <h1 className="display-1 fw-bold">404</h1>
           <p className="fs-3"> <span className="text-danger">Opps!</span> Página não encontrada.</p>
           <p className="lead">
-            O link que você digitou não existe. Por valor Verifique o endereço digitado.
+            O link que você digitou não existe. Por valor verifique o endereço digitado.
           </p>
           <Link to='/'>
             <ComponentButtonCommon text='Ir para a página inicial' />
           </Link>
         </div>
       </div>
-      <Footer />
     </div>
   </>
 )

ca029a9f9000684f7290e1614fbb3cab2140ca1b
edmooliveira29 <edmooliveira29@gmail.com>
2023-09-11 18:38:45 -0300

    feat: update packages

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'jsdom',
-	rootDir: 'src',
-	verbose: true,
-	collectCoverage: true,
-	coverageDirectory: '../coverage',
-	setupFilesAfterEnv: ['@testing-library/jest-dom'],
-};

4fc8bb4025379ae9d587c448978df565389b35a3
edmo <edmooliveira29@gmail.com>
2024-01-09 17:49:35 -0300

    fix: updated .yml

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     video: false,
     screenshotOnRunFailure: false,
     fixturesFolder: false,
-    defaultCommandTimeout: 10000
+    defaultCommandTimeout: 60000
   },
   component: {
     devServer: {

8663b6838a71c54d62c9ced9ca6a22a0871c6f79
edmo <edmooliveira29@gmail.com>
2024-02-07 16:26:20 -0300

    fix: updated tests

--- tests/e2e/cypress/e2e/my-account/my-account.cy.ts
+++ tests/e2e/cypress/e2e/my-account/my-account.cy.ts
@@ -22,7 +22,7 @@ describe('Create Account without Google', () => {
         cy.get('#navbar-toggler').click()
         cy.get('#link-login').click()
         cy.get('#navbar-toggler').click()
-      } 
+      }
       cy.get('#button-commom-register').click()
       cy.get('#input-name').type('Company Of Tests')
       cy.get('#input-email').type('company-tests@gmail.com')
@@ -83,10 +83,10 @@ describe('Create Account without Google', () => {
 
       cy.get('#img-user').click()
       cy.get('#my-account-dropdown').click()
-      if(i == 1) cy.get('#close-sideBar').click()
+      if (i == 1) cy.get('#close-sideBar').click()
       cy.get('#input-cnpj').type('06135538000190')
       cy.wait(2000)
-      if(i == 0) cy.get('body').trigger('keydown', { keyCode: 27, which: 27 })
+      if (i == 0) cy.get('body').trigger('keydown', { keyCode: 27, which: 27 })
       cy.get('#button-success-save-edit-profile').click()
       cy.get('#message-alert').should('have.text', 'Empresa editada com sucesso')
       cy.get('#confirm-text-ok').click()
@@ -99,6 +99,70 @@ describe('Create Account without Google', () => {
 
     })
 
-  
+    it('Assert expiration date in this day ' + (i == 0 ? ' - desktop' : ' - mobile'), () => {
+      cy.wait(1000)
+      if (i == 0) {
+        cy.viewport(1920, 1080)
+        cy.get('#link-login').click()
+      } else {
+        cy.viewport(412, 915)
+        cy.get('#navbar-toggler').click()
+        cy.get('#link-login').click()
+        cy.get('#navbar-toggler').click()
+      }
+      cy.get('#input-username').type('company-tests')
+      cy.get('#input-password').type('1234*Abcd')
+      cy.wait(1000)
+      cy.get('#button-login').click()
+
+      cy.get('#expire-session').should('have.text', `Sua sessão expira no dia ${new Date().toLocaleDateString('pt-BR', { timeZone: 'America/Sao_Paulo' })} às 23:59:00`)
+      cy.get('#img-user').click()
+      cy.get('#logout-system').click()
+      cy.get('#confirm-text').click()
+      cy.get('#text-logout').should('be.visible')
+      cy.get('#text-logout').should('have.text', 'Saindo...')
+      cy.wait(2000)
+
+    })
+
+    it('Assert expiration date in 7 days ' + (i == 0 ? ' - desktop' : ' - mobile'), () => {
+      cy.wait(1000)
+      if (i == 0) {
+        cy.viewport(1920, 1080)
+        cy.get('#link-login').click()
+      } else {
+        cy.viewport(412, 915)
+        cy.get('#navbar-toggler').click()
+        cy.get('#link-login').click()
+        cy.get('#navbar-toggler').click()
+      }
+      cy.get('#input-username').type('company-tests')
+      cy.get('#input-password').type('1234*Abcd')
+      cy.get('#checkbox-input-remember').click()
+      cy.wait(1000)
+      cy.get('#button-login').click()
+
+
+      const nowDate = new Date()
+
+      const dateFuture = new Date(nowDate)
+      dateFuture.setDate(nowDate.getDate() + 7)
+      const dateFormated = dateFuture.toLocaleDateString('pt-BR', { timeZone: 'America/Sao_Paulo' })
+      dateFuture.setSeconds(dateFuture.getSeconds() + 1)
+
+      cy.get('#expire-session').should(($element) => {
+        const fullText = $element.text()
+        const expectedText = `Sua sessão expira no dia ${dateFormated}`
+
+        expect(fullText).to.include(`Sua sessão expira no dia ${dateFormated}`)
+      })
+      cy.get('#img-user').click()
+      cy.get('#logout-system').click()
+      cy.get('#confirm-text').click()
+      cy.get('#text-logout').should('be.visible')
+      cy.get('#text-logout').should('have.text', 'Saindo...')
+      cy.wait(2000)
+
+    })
   }
 })
